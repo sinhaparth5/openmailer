3e0bca0be542914b1e466a0dab25f1065461c229
Parth <parth_sinha18@proton.me>
2025-10-11 20:07:01 +0100

    updated contact list memebers table

--- database/migrations/2025_10_11_182755_create_contact_list_members_table.php
+++ database/migrations/2025_10_11_182755_create_contact_list_members_table.php
@@ -12,8 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('contact_list_members', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->foreignUuid('contact_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('contact_list_id')->constrained()->onDelete('cascade');
             $table->timestamps();
+
+            $table->unique(['contact_id', 'contact_list_id']);
         });
     }
 
