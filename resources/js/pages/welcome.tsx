aa53e258ce8d9b1494f7e8b9f2b013473f6e0203
Parth <parth_sinha18@proton.me>
2025-10-11 21:44:14 +0100

    updated page title

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -7,7 +7,7 @@ export default function Welcome() {
 
     return (
         <>
-            <Head title="Welcome">
+            <Head title="Openmailer">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link
                     href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"
