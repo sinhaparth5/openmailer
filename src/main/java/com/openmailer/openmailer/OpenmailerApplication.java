cfd562118601f56cf19d314a4e9e9b77ecda2d7d
Parth Sinha <parth.sinha@webcurl.co.uk>
2025-12-16 10:18:20 +0000

    added openmailer project

---
+++ src/main/java/com/openmailer/openmailer/OpenmailerApplication.java
@@ -0,0 +1,13 @@
+package com.openmailer.openmailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenmailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenmailerApplication.class, args);
+	}
+
+}
